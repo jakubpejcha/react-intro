c1fbe818283b5cdb74bb260b97a60f08766c7d1b
jakubpejcha <57291294+jakubpejcha@users.noreply.github.com>
2020-08-07 14:03:08 +0200

    Adding data mapping lesson.

--- src_styles/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-//import Appexc from "./components_exc/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// excercise
-//ReactDOM.render(<Appexc />, document.getElementById("root"));
