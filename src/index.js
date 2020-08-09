6f861c56a3fd6841a192dfac9f92ea09fed2a691
jakubpejcha <57291294+jakubpejcha@users.noreply.github.com>
2020-08-09 16:48:25 +0200

    Adding state.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,28 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Declarative vs imperative
+/*
+// declarative
+let isDone = true;
+const lineThrough = {textDecoration: 'line-through'}
+
+ReactDOM.render(
+  <p style={isDone ? lineThrough : null}>Buy milk</p>,
+  document.getElementById("root"),
+);
+
+// imperative
+function strike() {
+  document.getElementById('item').style.textDecoration = 'line-through';
+}
+
+ReactDOM.render(
+  <div>
+    <p id="item">Buy milk</p>,
+    <button onClick={strike}>Strike through!</button>
+  </div>,
+  document.getElementById("root"),
+);
+*/
