bac5a176c6f1b1bf6e9d0d9dbddd9e86c3c32384
jakubpejcha <57291294+jakubpejcha@users.noreply.github.com>
2020-08-11 20:21:43 +0200

    Adding destructuring excercise.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,57 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// destructuring excercise
+const cars = [
+  {
+    model: "Honda Civic",
+    //The top colour refers to the first item in the array below:
+    //i.e. hondaTopColour = "black"
+    coloursByPopularity: ["black", "silver"],
+    speedStats: {
+      topSpeed: 140,
+      zeroToSixty: 8.5
+    }
+  },
+  {
+    model: "Tesla Model 3",
+    coloursByPopularity: ["red", "white"],
+    speedStats: {
+      topSpeed: 150,
+      zeroToSixty: 3.2
+    }
+  }
+];
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
