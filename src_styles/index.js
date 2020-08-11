d5f26cfceb64f8abc90a2d7836648527c2ef1ce1
jakubpejcha <57291294+jakubpejcha@users.noreply.github.com>
2020-08-11 19:41:56 +0200

    useState excercisse.

--- src_styles/index.js
+++ src_styles/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+//import Appexc from "./components_exc/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// excercise
+//ReactDOM.render(<Appexc />, document.getElementById("root"));
