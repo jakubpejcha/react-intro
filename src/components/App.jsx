a9354c09660676660377491f129000b29c708149
jakubpejcha <57291294+jakubpejcha@users.noreply.github.com>
2020-08-14 13:08:52 +0200

    Adding spread op. excersice.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,115 +1,42 @@
 import React, { useState } from "react";
-
-function App_() {
-  const [fullName, setName] = useState({
-    fName: "",
-    lName: ""
-  });
-
-  function handleOnChange(event) {
-    const { value, name } = event.target;
-
-    setName((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName
-        };
-      } else {
-        return {
-          fName: prevValue.fName,
-          lName: value
-        };
-      }
-    });
-  }
-
-  return (
-    <div className="container">
-      <h1>
-        Hello {fullName.fName} {fullName.lName}
-      </h1>
-      <form>
-        <input
-          onChange={handleOnChange}
-          name="fName"
-          placeholder="First Name"
-          value={fullName.fName}
-        />
-        <input
-          onChange={handleOnChange}
-          name="lName"
-          placeholder="Last Name"
-          value={fullName.lName}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
-}
-
-// cviko
+//import items from "../items";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-  function handleOnChange(event) {
-    const { value, name } = event.target;
+  function handleChange(event) {
+    const value = event.target.value;
 
-    setContact((prevState) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevState.lName,
-          email: prevState.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevState.fName,
-          lName: value,
-          email: prevState.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevState.fName,
-          lName: prevState.lName,
-          email: value
-        };
-      }
+    setInputText(value);
+  }
+
+  function handleClick() {
+    setItems((prevValue) => {
+      return [...prevValue, inputText];
     });
+
+    setInputText("");
   }
 
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleOnChange}
-          name="fName"
-          placeholder="First Name"
-          value={contact.fName}
-        />
-        <input
-          onChange={handleOnChange}
-          name="lName"
-          placeholder="Last Name"
-          value={contact.lName}
-        />
-        <input
-          onChange={handleOnChange}
-          name="email"
-          placeholder="Email"
-          value={contact.email}
-        />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
